70fcf9db66ec0d85b8705b9550432bc97b09732d
Mhattzzz <84877905+mhattzzz@users.noreply.github.com>
2022-11-01 16:02:32 +0100

    Add files via upload

---
+++ valorantids.h
@@ -0,0 +1,93 @@
+// agents ids // missing last added agent
+switch (id)
+{
+case 15336611:
+	return "Astra NPC";
+case 15335538:
+	return "Astra";
+case 15321378:
+	return "Breach";
+case 15336891:
+	return "Brimstone";
+case 15323988:
+	return "Chamber";
+case 15328948:
+	return "Cypher";
+case 15319792:
+	return "Fade";
+case 15343159:
+	return "Jett";
+case 15326591:
+	return "Kayo";
+case 15331443:
+	return "Killjoy";
+case 15337809:
+	return "Neon";
+case 15342356:
+	return "Omen";
+case 15334560:
+	return "Phoenix";
+case 15321953:
+	return "Raze";
+case 15341565:
+	return "Reyna";
+case 15340716:
+	return "Sage";
+case 15330160:
+	return "Sova";
+case 15333452:
+	return "Viper";
+case 15339645:
+	return "Yoru";
+case 15327696:
+	return "Skye";
+case 15341518:
+	return "Trial Bot";
+default:
+	return std::to_string(id);
+}
+
+// guns ids
+switch (id)
+{
+case 15523324:
+	return "Knife";
+case 15341507:
+	return "Classic";
+case 15471847:
+	return "Shorty";
+case 15441711:
+	return "Frenzy";
+case 15457094:
+	return "Ghost";
+case 15464006:
+	return "Sherif";
+case 15512670:
+	return "Stinger";
+case 15501837:
+	return "Spectre";
+case 15434830:
+	return "Bucky";
+case 15429095:
+	return "Judge";
+case 15411129:
+	return "Bulldog";
+case 15487507:
+	return "Guardian";
+case 15416268:
+	return "Phantom";
+case 15400141:
+	return "Vandal";
+case 15495978:
+	return "Marshal";
+case 15475738:
+	return "Operator";
+case 15394615:
+	return "Ares";
+case 15390042:
+	return "Odin";
+default:
+	return "Ability";
+	//return std::to_string(id);
+	break;
+}
